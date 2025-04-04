afe6a5d9a07c04f21ca516d4afcba49c8a8c6c5e
gabriel-vieira-chagas <gabrielvieirachagas@gmail.com>
2025-04-04 18:33:01 -0300

    exercicio06 commit

---
+++ exercicio06/Aluno.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio6 //Define o namespace para chamada no Program
+{
+    internal class Aluno // Declara classe Aluno
+    {
+        private string Nome { get; set; } //Armazena nome do aluno
+        private int Matricula { get; set; } //Armazena matrícula do aluno
+        private string Curso { get; set; } //Armazena curso do aluno
+        private double Media { get; set; } //Armazena média do aluno
+
+        public Aluno(string nome, int matricula, string curso, double media) //Cria construtor da classe que recebe e atribui os dados
+        {
+            Nome = nome;
+            Matricula = matricula;
+            Curso = curso;
+            Media = media;
+            //Atribuição de parâmetro à propriedade
+        }
+
+        public void ExibirDados() //Método de exibição requisitado
+        {
+            Console.WriteLine("Exibição de dados do aluno:"); //Exibição dos dados
+            Console.WriteLine($"Nome: {Nome}"); //Exibe nome
+            Console.WriteLine($"Matrícula: {Matricula}"); //Exibe matrícula
+            Console.WriteLine($"Curso: {Curso}"); //Exibe curso
+            Console.WriteLine($"Média das Notas: {Media}\n"); //Exibe nota
+        }
+
+        public void VerificarAprovacao() //Método de verificar aprovação requisitado
+        {
+            if (Media >= 7) //Verifica se a média é maior ou igual a sete
+            {
+                Console.WriteLine($"Aprovação de {Nome}: Aprovado\n"); //Exibe resposta de aprovado
+            }
+            else
+            {
+                Console.WriteLine($"Aprovação de {Nome}: Reprovado\n"); //Exibe resposta de reprovado
+            }
+        }
+    }
+}
---
+++ exercicio06/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using exercicio6; //Importa namespace para manipular classe aluno
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Cria objeto gabriel com os dados deste aluno específico
+        Aluno gabriel = new Aluno("Gabriel Vieira Chagas", 1234, "Engenharia de Software", 9.8);
+        //Cria objeto giovani com os dados deste aluno específico
+        Aluno giovani = new Aluno("Giovani Marlon", 5678, "Artes Cênicas", 5.4);
+
+        gabriel.ExibirDados(); //Método que exibe dados desse aluno específico
+        gabriel.VerificarAprovacao(); //Método que verifica aprovação desse aluno específico
+
+        giovani.ExibirDados(); //Método que exibe dados desse aluno específico
+        giovani.VerificarAprovacao(); //Método que verifica aprovação desse aluno específico
+    }
+}
+
+/*
+ Em Aluno.cs, o código começa atribuindo o namespace do exercício para ser chamado no Program.
+Em seguida, é declarada a classe Aluno, armazenando os dados requisitados com get e set,
+permitindo acessar e modificar os valores quando necessário. Get permite ler a propriedade e set permite alterá-la.
+É criado então o construtor para poder atribuir os parâmetros às propriedades.
+No exemplo do objeto “gabriel” o construtor é chamado e atribui o parâmetro “Gabriel Vieira Chagas” à propriedade “Nome”.
+São criados na sequência ambos os métodos ExibirDados() e VerificarAprovacao(), que fazem respectivamente,
+exibir os dados preenchidos do aluno e verifica através de um if simples se a média é maior do que sete,
+para retornar aprovação ou reprovação do aluno.
+No Program.cs, o namespace que contém a classe é chamado inicialmente,
+e no Main() são criados os objetos de dois alunos com seus respectivos dados.
+Por fim, são chamados ambos os métodos para cada aluno, exibindo as informações de dados e aprovação.
+ */
