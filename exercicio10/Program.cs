cae972f990eda7b4ac54f4e770adc9b962ad911e
gabriel-vieira-chagas <gabrielvieirachagas@gmail.com>
2025-04-04 20:21:20 -0300

    commit ex10

--- exercicio10/Program.cs
+++ exercicio10/Program.cs
@@ -4,20 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random random = new Random();
-        int numeroSecreto = random.Next(1, 51);
+        Random random = new Random(); //Declara variável random para instanciar a classe nativa Random do System 
+        int numeroSecreto = random.Next(1, 51); //Gera um numero aleatorio no intervalo de 1 a 51, sendo o numero 51 exclusivo
         int palpite = 0; // Inicializa variável de palpite
         int chances = 1; //Inicializa variável de quantidade de chances
         bool condicaoAcerto = false; //Inicializa variável de condição de acerto
         bool condicaoChances = false; //Inicializa variável de condição de chance
+        bool tratamentoEntrada = false; //Inicializa tratamento de entrada
 
         Console.WriteLine($"-= Num: {numeroSecreto} =- "); //Exibe o número secreto para facilitar depuração
 
         Console.WriteLine("Você tem 5 chances para advinhar o número secreto de 1 a 50!\n"); //Mensagem de exibição inicial
         while (condicaoChances == false && condicaoAcerto == false) //Cria while para que o loop pare caso acerte ou acabe as chances
         {
-            Console.Write($"Chance n° {chances}: "); //Exibe numero de chances e requisita tentativa
-            palpite = Convert.ToInt32(Console.ReadLine()); //Recebe tentativa
+            while (tratamentoEntrada == false){
+                Console.Write($"Chance n° {chances}: "); //Exibe numero de chances e requisita tentativa
+                try //Verifica se o que foi digitado é um número inteiro válido
+                {
+                    palpite = Convert.ToInt32(Console.ReadLine()); //Recebe tentativa
+                    if (palpite > 50 || palpite < 0) //Verifica se o palpite é maior 50 ou menor que 0
+                    {
+                        Console.WriteLine("Por favor, digite um número inteiro de 1 a 50.\n"); //Exibe erro
+                    }
+                    else
+                    {
+                        tratamentoEntrada = true; //Permite que a entrada passe
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Por favor, digite um número inteiro de 1 a 50.\n"); //Exibe erro
+                }
+            }
 
             if (palpite < numeroSecreto) //Caso palpite seja menor exibe uma mensagem
             {
@@ -39,7 +57,8 @@ class Program
             {
                 condicaoAcerto = true;
             }
-            chances++;
+            chances++; //Aumenta o contador de chances
+            tratamentoEntrada = false; //Reinicia o processo de tratamento de entrada
         }
         if (condicaoAcerto) //Caso tenha acertado exibe mensagem de acerto
         {
